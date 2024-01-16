cc367bc958f3cde8bc4d722d3a0292fb3d3aeb2b
Myrdrahl <even.juberg@artsdatabanken.no>
2024-01-16 12:35:36 +0100

    fixing my oops

--- tree.sh
+++ tree.sh
@@ -1,7 +1,7 @@
-      echo "list folder"
-      ls -l
-      echo "enter code"
-      cd code  
+      cd code
+      mv index.html index.bk
       find . -type d -exec tree -H . -o  '{}/index.html' \;
+      rm index.html
+      mv index.bk index.html
       cd ..
       ls -l
